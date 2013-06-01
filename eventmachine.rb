ab2b6c734be95610125b1476efc269fce7867c01
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-06-01 11:03:11 +0530

    added another client to demonstrate how pub/sub works with more than one subscriber. Cleaned out indentation as well

--- eventmachine.rb
+++ eventmachine.rb
@@ -2,11 +2,10 @@ require 'faye'
 require 'eventmachine'
 
 EM.run {
-  client = Faye::Client.new('http://localhost:9292/faye')
+	
+	client = Faye::Client.new('http://localhost:9292/faye')
 
-  client.subscribe('/browser') do |message|
-    puts message.inspect
-  	client.publish('/server', 'text' => 'Hello from server')
-  end
-
+	client.subscribe('/browser') do |message|
+		client.publish('/server', 'text' => 'Hello subscribers, this if your server')
+	end
 }
